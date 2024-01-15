8ded783c91a6a2fa65e3221c3578bf34cacfaef0
Landon Schropp <schroppl@gmail.com>
2024-01-14 22:18:17 -0800

    Add distanceWithDiagonals function

---
+++ src/square/distance-with-diagonals.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "bun:test";
+import { distanceWithDiagonals } from "./distance-with-diagonals";
+
+describe("distanceWithDiagonals", () => {
+  it("returns the number of squares a rook would travel between two squares", () => {
+    expect(distanceWithDiagonals("c2", "g4")).toEqual(4);
+    expect(distanceWithDiagonals("g4", "c2")).toEqual(4);
+    expect(distanceWithDiagonals("c4", "g2")).toEqual(4);
+    expect(distanceWithDiagonals("g2", "c4")).toEqual(4);
+  });
+});
---
+++ src/square/distance-with-diagonals.ts
@@ -0,0 +1,15 @@
+import { Square } from "../types";
+import { getVectorBetweenSquares } from "./get-vector-between-squares";
+
+/**
+ * The distance between two squares on the chessboard with diagonal moves. This is the number of
+ * squares a king would have to move to get from one square to another. In mathematical terms, this
+ * is known as the [Chebyshev distance](https://en.wikipedia.org/wiki/Chebyshev_distance).
+ * @param square1 The first square.
+ * @param square2 The second square.
+ * @returns This distance between the two squares with diagonal moves.
+ */
+export function distanceWithDiagonals(square1: Square, square2: Square): number {
+  const vector = getVectorBetweenSquares(square1, square2);
+  return Math.max(Math.abs(vector[0]), Math.abs(vector[1]));
+}
