420eae9ef6bfdc79233e80e69ab1a2ffd7a6da76
Landon Schropp <schroppl@gmail.com>
2024-01-15 22:58:54 -0800

    Refactor getBoard
    The function now returns the square as well as the piece

--- src/fen/get-board.ts
+++ src/fen/get-board.ts
@@ -1,17 +1,25 @@
 import { map, pipe, reverse } from "remeda";
-import { Piece } from "../types";
+import { BOARD_SIZE } from "../constants";
+import { convertCoordinatesToSquare } from "../square";
+import { Piece, Square } from "../types";
 import { chessJsPieceToPiece, createChess } from "../utilities/chess-js";
 
 /**
  * Returns a 2D array of the board.
  * @param fen The FEN to parse.
- * @returns A 2D array of the board with "a1" as the `[0][0]` element. Empty squares are represented
- * as `null`.
+ * @returns A 2D array of the board. The coordinates of the board use "a1" as the `[0][0]` element.
+ * Each square contains an object, with a `square` property and a `piece` property. Empty squares
+ * are represented with `null`.
  */
-export function getBoard(fen: string): (Piece | null)[][] {
+export function getBoard(fen: string): { square: Square; piece: Piece | null }[][] {
   return pipe(
     createChess(fen).board(),
-    map(map((piece) => piece && chessJsPieceToPiece(piece))),
+    map.indexed((rank, rankIndex) => {
+      return rank.map((piece, fileIndex) => ({
+        square: convertCoordinatesToSquare([fileIndex, BOARD_SIZE - rankIndex - 1]),
+        piece: piece && chessJsPieceToPiece(piece),
+      }));
+    }),
     reverse(),
   );
 }
