c4714f37f65fb5b23fc91906f462fc1d61782933
Landon Schropp <schroppl@gmail.com>
2024-01-16 20:39:12 -0800

    Add getPiece function

---
+++ src/fen/get-piece.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "bun:test";
+import { STARTING_POSITION, WHITE_BISHOP } from "../constants";
+import { getPiece } from "./get-piece";
+
+describe("getPiece", () => {
+  describe("when the square has a piece", () => {
+    it("returns the piece", () => {
+      expect(getPiece(STARTING_POSITION, "c1")).toEqual(WHITE_BISHOP);
+    });
+  });
+
+  describe("when the square does not have a piece", () => {
+    it("returns null", () => {
+      expect(getPiece(STARTING_POSITION, "c3")).toBeNull();
+    });
+  });
+});
---
+++ src/fen/get-piece.ts
@@ -0,0 +1,13 @@
+import { Piece, Square } from "../types";
+import { chessJsPieceToPiece, createChess } from "../utilities/chess-js";
+
+/**
+ * Returns the piece at the square.
+ * @param fen The FEN to parse.
+ * @param square The square to get the piece from.
+ * @returns The piece on the square, or `null` if there is no piece.
+ */
+export function getPiece(fen: string, square: Square): Piece | null {
+  const piece = createChess(fen).get(square);
+  return piece ? chessJsPieceToPiece(piece) : null;
+}
