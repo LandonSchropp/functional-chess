f6c59a756a9502d3637bc75ebb06d7b24399914e
Landon Schropp <schroppl@gmail.com>
2024-01-17 19:35:56 -0800

    Add error tests

--- src/errors.ts
+++ src/errors.ts
@@ -1,9 +1,21 @@
 /**
- * This error indicates that the square in an operation was outside the bounds of a chessboard.
+ * The square in an operation was outside the bounds of a chessboard.
  */
 export class OutOfBoundsError extends Error {
   constructor(message: string) {
     super(message);
     this.message = message;
+    this.name = this.constructor.name;
+  }
+}
+
+/**
+ * The FEN string is invalid.
+ */
+export class InvalidFenError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.message = message;
+    this.name = this.constructor.name;
   }
 }
