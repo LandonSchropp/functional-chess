f5257eb4e45d768841028e4a65503b5e173ba669
Landon Schropp <schroppl@gmail.com>
2024-02-10 22:56:35 -0800

    Remove Chess.js and unneeded functions
    These will be replaced with new implementations based on 0x88.

--- src/fen/split-fen.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A utility function that splits a FEN into its parts.
- * @param fen The FEN to split.
- * @returns A tuple containing the parts of the FEN.
- * @private
- */
-export function splitFen(fen: string): [string, string, string, string, string, string] {
-  return fen.split(" ") as [string, string, string, string, string, string];
-}
