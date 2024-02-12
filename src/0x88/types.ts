8baac121e5b4f12356f689d1a30b1da1594850ff
Landon Schropp <schroppl@gmail.com>
2024-02-11 22:23:19 -0800

    Add 0x88 constants and types

---
+++ src/0x88/types.ts
@@ -0,0 +1,17 @@
+import { PIECES_0x88, SQUARES_0x88 } from "./constants";
+
+/**
+ * A 0x88 game board.
+ * @private
+ */
+export type Board0x88 = Uint8Array;
+
+/**
+ * A 0x88 square.
+ */
+export type Square0x88 = (typeof SQUARES_0x88)[number];
+
+/**
+ * A 0x88 piece.
+ */
+export type Piece0x88 = (typeof PIECES_0x88)[number];
