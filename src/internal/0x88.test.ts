9d0a99805a54fe6553c491a2f0c7f8094e285afe
Landon Schropp <schroppl@gmail.com>
2024-02-13 18:30:58 -0800

    Implement internal parseFen function

--- src/internal/0x88.test.ts
+++ src/internal/0x88.test.ts
@@ -1,5 +1,6 @@
-import { BOARD_SIZE } from "../constants";
-import { toAscii, toUnicode } from "./0x88";
+import { BOARD_SIZE, STARTING_POSITION, WHITE_PAWN } from "../constants";
+import { InvalidFenError } from "../errors";
+import { parseFen, toAscii, toUnicode } from "./0x88";
 import {
   BLACK_BISHOP_0x88,
   BLACK_KING_0x88,
@@ -45,6 +46,9 @@ const STARTING_POSITION_BOARD_0x88 = new Uint8Array([
   ...Array.from({ length: BOARD_WIDTH_0x88 - BOARD_SIZE }, () => EMPTY_SQUARE_0x88),
 ]);
 
+// TODO: Fix this when Bun released the fix for the issue #8243
+const TypedInvalidFenError = InvalidFenError as unknown as Error;
+
 describe("toAscii", () => {
   it("returns the ASCII string representation of a board", () => {
     const expected = dedent`
@@ -76,5 +80,74 @@ describe("toUnicode", () => {
     `;
 
     expect(toUnicode(STARTING_POSITION_BOARD_0x88)).toBe(expected);
+  });
+});
+
+describe("parseFen", () => {
+  describe("when the FEN has less than 6 parts", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen = STARTING_POSITION.replace(/1$/, "");
+      expect(() => parseFen(fen)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when the FEN has more than 6 parts", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen = `${STARTING_POSITION} 1`;
+      expect(() => parseFen(fen)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when the position contains invalid characters", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen = STARTING_POSITION.replace(WHITE_PAWN, "x");
+      expect(() => parseFen(fen)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when the position has too few ranks", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen = STARTING_POSITION.replace("8/", "");
+      expect(() => parseFen(fen)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when the position has too many ranks", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen = STARTING_POSITION.replace("8", "8/8");
+      expect(() => parseFen(fen)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when one of the position's ranks contains too few pieces and empty squares", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen1 = STARTING_POSITION.replace("8", "7");
+      expect(() => parseFen(fen1)).toThrow(TypedInvalidFenError);
+
+      const fen2 = STARTING_POSITION.replace(WHITE_PAWN, "");
+      expect(() => parseFen(fen2)).toThrow(TypedInvalidFenError);
+
+      const fen3 = STARTING_POSITION.replace("8", `2${WHITE_PAWN}2${WHITE_PAWN}1`);
+      expect(() => parseFen(fen3)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when one of the position's ranks contains too many pieces and empty squares", () => {
+    it("raises an UnparsableFenError", () => {
+      const fen1 = STARTING_POSITION.replace("8", "9");
+      expect(() => parseFen(fen1)).toThrow(TypedInvalidFenError);
+
+      const fen2 = STARTING_POSITION.replace(WHITE_PAWN, `${WHITE_PAWN}${WHITE_PAWN}`);
+      expect(() => parseFen(fen2)).toThrow(TypedInvalidFenError);
+
+      const fen3 = STARTING_POSITION.replace("8", `2${WHITE_PAWN}2${WHITE_PAWN}3`);
+      expect(() => parseFen(fen3)).toThrow(TypedInvalidFenError);
+    });
+  });
+
+  describe("when the FEN is valid", () => {
+    it("parses the FEN", () => {
+      expect(parseFen(STARTING_POSITION)).toEqual(STARTING_POSITION_BOARD_0x88);
+    });
   });
 });
--- src/internal/0x88.ts
+++ src/internal/0x88.ts
@@ -1,10 +1,40 @@
 import { BOARD_SIZE } from "../constants";
+import { InvalidFenError } from "../errors";
 import {
   BOARD_WIDTH_0x88,
+  EMPTY_SQUARE_0x88,
   PIECE_0x88_TO_PIECE_ASCII,
   PIECE_0x88_TO_PIECE_UNICODE,
+  PIECE_TO_PIECE_0x88,
 } from "../internal/constants";
 import { Board0x88, EmptySquare0x88, Piece0x88 } from "../internal/types";
+import { isPiece } from "../types";
+
+/**
+ * A regular expression to match a FEN string. This is not meant to be comprehensive.
+ */
+const FEN_REGEX = new RegExp(
+  [
+    "^",
+    "([1-8pnbrqkPNBRQK/]+)",
+    "\\s+",
+    "([wb])",
+    "\\s+",
+    "([kqKQ]+|-)",
+    "\\s+",
+    "([a-h][1-8]|-)",
+    "\\s+",
+    "(\\d+)",
+    "\\s+",
+    "(\\d+)",
+    "$",
+  ].join(""),
+);
+
+const UNOCCUPIED_SQUARES = Array.from(
+  { length: BOARD_WIDTH_0x88 - BOARD_SIZE },
+  () => EMPTY_SQUARE_0x88,
+) as EmptySquare0x88[];
 
 /**
  * Returns the index of a square for an 0x88 board.
@@ -63,4 +93,57 @@ export function toUnicode(board: Board0x88): string {
       );
     }).join("");
   }).join("\n");
+}
+
+function parseRank(fen: string, rank: string): (Piece0x88 | EmptySquare0x88)[] {
+  const pieces: (Piece0x88 | EmptySquare0x88)[] = rank.split("").flatMap((char) => {
+    if (isPiece(char)) {
+      return PIECE_TO_PIECE_0x88[char];
+    }
+
+    if (/[1-8]/.test(char)) {
+      return Array.from({ length: Number(char) }, () => EMPTY_SQUARE_0x88);
+    }
+
+    throw new InvalidFenError(
+      `The position in the FEN '${fen}' contains the invalid character '${char}'.`,
+    );
+  });
+
+  if (pieces.length !== BOARD_SIZE) {
+    throw new InvalidFenError(
+      `The pieces and empty squares in one of the ranks in the FEN '${fen}' doesn't add up ` +
+        `to ${BOARD_SIZE}.`,
+    );
+  }
+
+  return [...pieces, ...UNOCCUPIED_SQUARES];
+}
+
+/**
+ * Parses a FEN string into a 0x88 board.
+ * @param fen - The FEN string.
+ * @returns The 0x88 board.
+ */
+// TODO: This should represent the complete FEN state, not just the position.
+export function parseFen(fen: string): Board0x88 {
+  if (!FEN_REGEX.test(fen)) {
+    throw new InvalidFenError(`The FEN string '${fen}' could not be parsed.`);
+  }
+
+  const position = fen.split(/\s+/)[0];
+
+  const board = new Uint8Array(
+    position
+      .split("/")
+      .map((rank) => parseRank(fen, rank))
+      .toReversed()
+      .flat(),
+  );
+
+  if (board.length !== BOARD_WIDTH_0x88 * BOARD_SIZE) {
+    throw new InvalidFenError(`The FEN '${fen}' does not have ${BOARD_SIZE} ranks.`);
+  }
+
+  return board;
 }
