5634667c40ff675036c3588e148c3739e38e1c71
Landon Schropp <schroppl@gmail.com>
2024-02-15 23:29:34 -0800

    Remove internal index

--- src/internal/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./constants";
-export * from "./derive-from-fen";
-export * from "./readonly-map";
-export * from "./transform-fen";
-export * from "./types";
-export * from "./vector";
