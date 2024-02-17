27da0f04757154e24e6da9127e4c8fd53bb464c8
Landon Schropp <schroppl@gmail.com>
2024-02-16 22:17:35 -0800

    Remove getPieceFromIndices0x88 function

--- src/fen/to-unicode.ts
+++ src/fen/to-unicode.ts
@@ -15,7 +15,7 @@ import {
   WHITE_ROOK_0x88,
 } from "../internal/constants";
 import { deriveFromFen } from "../internal/derive-from-fen";
-import { getPieceFromIndices0x88 } from "../internal/get-piece-from-indices-0x88";
+import { getSquareFromIndices0x88 } from "../internal/get-square-from-indices-0x88";
 import { invert } from "../internal/readonly-map";
 import { Fen0x88 } from "../internal/types";
 
@@ -45,7 +45,7 @@ export const toUnicode = deriveFromFen((fen: Fen0x88): string => {
   return Array.from({ length: BOARD_SIZE }, (_, rankIndex) => {
     return Array.from({ length: BOARD_SIZE }, (_, fileIndex) => {
       return PIECE_0x88_TO_PIECE_UNICODE.get(
-        getPieceFromIndices0x88(fen[0], BOARD_SIZE - rankIndex - 1, fileIndex),
+        fen[0][getSquareFromIndices0x88(BOARD_SIZE - rankIndex - 1, fileIndex)],
       );
     }).join("");
   }).join("\n");
