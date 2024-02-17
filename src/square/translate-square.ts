1b8f7fabe1f9d836342962e26af2b25d75ed9b3c
Landon Schropp <schroppl@gmail.com>
2024-02-16 23:52:59 -0800

    Rename parameter

--- src/square/translate-square.ts
+++ src/square/translate-square.ts
@@ -10,17 +10,17 @@ import { convertSquareToCoordinates } from "./convert-square-to-coordinates";
  * @example TranslateSquare("e5", [1, 2]); // "f7"
  *
  * @param square The square to translate.
- * @param vector A vector indicating how to translate the file and rank of the square.
+ * @param translation A vector indicating how to translate the file and rank of the square.
  * @returns The new square after applying the translation.
  * @throws Throws an error if the newly translated square is out of bounds.
  */
-export function translateSquare(square: Square, vector: Vector) {
+export function translateSquare(square: Square, translation: Vector) {
   const [file, rank] = convertSquareToCoordinates(square);
-  const translatedCoordinates: Vector = [file + vector[0], rank + vector[1]];
+  const translatedCoordinates: Vector = [file + translation[0], rank + translation[1]];
 
   if (!isVectorInBounds(translatedCoordinates)) {
     throw new OutOfBoundsError(
-      `When the square ${square} is translated by the vector ${vector}, the resulting ` +
+      `When the square ${square} is translated by the vector ${translation}, the resulting ` +
         `coordinates are out of bounds: [${translatedCoordinates[0]}, ${translatedCoordinates[1]}]`,
     );
   }
