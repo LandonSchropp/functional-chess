76bdcfdc61da9ac6e5a0ca4ae267f13e1959171e
Landon Schropp <schroppl@gmail.com>
2024-03-30 21:29:31 -0700

    Clean up the knight move test descriptions

--- src/internal/get-legal-moves-0x88/knight-moves.test.ts
+++ src/internal/get-legal-moves-0x88/knight-moves.test.ts
@@ -37,7 +37,7 @@ import { expect, describe, it } from "bun:test";
 
 describe("getLegalMoves0x88 (knight moves)", () => {
   describe("when the knight is in the center of the board", () => {
-    describe("when the sqaures are empty", () => {
+    describe("when the target squares are empty", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("7k/8/8/4n3/4N3/8/8/7K w - - 0 1");
@@ -73,7 +73,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the opponent", () => {
+    describe("when the target squares are occupied by the opponent", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("7k/3R1R2/2Rr1rR1/2r1n1r1/2R1N1R1/2rR1Rr1/3r1r2/7K w - - 0 1");
@@ -109,7 +109,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the player", () => {
+    describe("when the target squares are occupied by the player", () => {
       describe("when the player is white", () => {
         it("returns an empty array", () => {
           const fen = parseFen("7k/3r1r2/2rR1Rr1/2R1n1R1/2r1N1r1/2Rr1rR1/3R1R2/7K w - - 0 1");
@@ -129,7 +129,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
   });
 
   describe("when the knight is on the edge of the board", () => {
-    describe("when the sqaures are empty", () => {
+    describe("when the target squares are empty", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("7k/8/8/n7/N7/8/8/7K w - - 0 1");
@@ -157,7 +157,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the opponent", () => {
+    describe("when the target squares are occupied by the opponent", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("7k/1R6/1rR5/n1r5/N1R5/1Rr5/1r6/7K w - - 0 1");
@@ -185,7 +185,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the player", () => {
+    describe("when the target squares are occupied by the player", () => {
       describe("when the player is white", () => {
         it("returns an empty array", () => {
           const fen = parseFen("7k/1r6/1Rr5/n1R5/N1r5/1rR5/1R6/7K w - - 0 1");
@@ -205,7 +205,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
   });
 
   describe("when the knight is in the corner of the board", () => {
-    describe("when the sqaures are empty", () => {
+    describe("when the target squares are empty", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("n6k/8/8/8/8/8/8/N6K w - - 0 1");
@@ -229,7 +229,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the opponent", () => {
+    describe("when the target squares are occupied by the opponent", () => {
       describe("when the player is white", () => {
         it("returns the knight moves", () => {
           const fen = parseFen("n6k/2R5/1R6/8/8/1r6/2r5/N6K w - - 0 1");
@@ -253,7 +253,7 @@ describe("getLegalMoves0x88 (knight moves)", () => {
       });
     });
 
-    describe("when the squares are occupied by the player", () => {
+    describe("when the target squares are occupied by the player", () => {
       describe("when the player is white", () => {
         it("returns an empty array", () => {
           const fen = parseFen("n6k/2r5/1r6/8/8/1R6/2R5/N6K w - - 0 1");
