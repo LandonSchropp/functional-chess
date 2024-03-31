83116a269a6d0242fd9910673e210112abeea5a7
Landon Schropp <schroppl@gmail.com>
2024-03-30 23:43:49 -0700

    Add missed pawn tests

--- src/internal/get-legal-moves-0x88/pawn-moves.test.ts
+++ src/internal/get-legal-moves-0x88/pawn-moves.test.ts
@@ -29,6 +29,10 @@ import {
   A5_0x88,
   H4_0x88,
   H5_0x88,
+  D1_0x88,
+  D8_0x88,
+  F1_0x88,
+  F8_0x88,
 } from "../constants";
 import { DOUBLE_PAWN_FLAG_0x88, CAPTURE_FLAG_0x88, EN_PASSANT_FLAG_0x88 } from "../constants/moves";
 import { encodeMove0x88 } from "../encode-move-0x88";
@@ -183,45 +187,105 @@ describe("getLegalMoves0x88 (pawn moves)", () => {
   });
 
   describe("when the pawn can legally capture to the left", () => {
-    describe("when the player is white", () => {
-      it("returns the capture", () => {
-        const fen = parseFen("8/k7/8/3pb3/4P3/8/K7/8 w - - 0 1");
+    describe("when the pawn is not on the rank before it would promote", () => {
+      describe("when the player is white", () => {
+        it("returns the capture", () => {
+          const fen = parseFen("8/k7/8/3pb3/4P3/8/K7/8 w - - 0 1");
 
-        expect(getLegalMoves0x88(fen, E4_0x88)).toMatchMoves([
-          encodeMove0x88(E4_0x88, D5_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
-        ]);
+          expect(getLegalMoves0x88(fen, E4_0x88)).toMatchMoves([
+            encodeMove0x88(E4_0x88, D5_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
+      });
+
+      describe("when the player is black", () => {
+        it("returns the capture", () => {
+          const fen = parseFen("8/k7/8/4p3/3PB3/8/K7/8 b - - 0 1");
+
+          expect(getLegalMoves0x88(fen, E5_0x88)).toMatchMoves([
+            encodeMove0x88(E5_0x88, D4_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
       });
     });
 
-    describe("when the player is black", () => {
-      it("returns the capture", () => {
-        const fen = parseFen("8/k7/8/4p3/3PB3/8/K7/8 b - - 0 1");
+    describe("when the pawn is on the rank before it would promote", () => {
+      describe("when the player is white", () => {
+        it("returns the capture with the promotions", () => {
+          const fen = parseFen("3rb3/k3P3/8/8/8/8/K7/8 w - - 0 1");
 
-        expect(getLegalMoves0x88(fen, E5_0x88)).toMatchMoves([
-          encodeMove0x88(E5_0x88, D4_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
-        ]);
+          expect(getLegalMoves0x88(fen, E7_0x88)).toMatchMoves([
+            encodeMove0x88(E7_0x88, D8_0x88, WHITE_KNIGHT_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, D8_0x88, WHITE_BISHOP_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, D8_0x88, WHITE_ROOK_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, D8_0x88, WHITE_QUEEN_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
+      });
+
+      describe("when the player is black", () => {
+        it("returns the capture with the pormotions", () => {
+          const fen = parseFen("8/k7/8/8/8/8/K3p3/3RB3 b - - 0 1");
+
+          expect(getLegalMoves0x88(fen, E2_0x88)).toMatchMoves([
+            encodeMove0x88(E2_0x88, D1_0x88, BLACK_KNIGHT_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, D1_0x88, BLACK_BISHOP_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, D1_0x88, BLACK_ROOK_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, D1_0x88, BLACK_QUEEN_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
       });
     });
   });
 
   describe("when the pawn can legally capture to the right", () => {
-    describe("when the player is white", () => {
-      it("returns the capture", () => {
-        const fen = parseFen("8/k7/8/4bp2/4P3/8/K7/8 w - - 0 1");
+    describe("when the pawn is not on the rank before it would promote", () => {
+      describe("when the player is white", () => {
+        it("returns the capture", () => {
+          const fen = parseFen("8/k7/8/4bp2/4P3/8/K7/8 w - - 0 1");
 
-        expect(getLegalMoves0x88(fen, E4_0x88)).toMatchMoves([
-          encodeMove0x88(E4_0x88, F5_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
-        ]);
+          expect(getLegalMoves0x88(fen, E4_0x88)).toMatchMoves([
+            encodeMove0x88(E4_0x88, F5_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
+      });
+
+      describe("when the player is black", () => {
+        it("returns the capture", () => {
+          const fen = parseFen("8/k7/8/4p3/4BP2/8/K7/8 b - - 0 1");
+
+          expect(getLegalMoves0x88(fen, E5_0x88)).toMatchMoves([
+            encodeMove0x88(E5_0x88, F4_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
       });
     });
 
-    describe("when the player is black", () => {
-      it("returns the capture", () => {
-        const fen = parseFen("8/k7/8/4p3/4BP2/8/K7/8 b - - 0 1");
+    describe("when the pawn is on the rank before it would promote", () => {
+      describe("when the player is white", () => {
+        it("returns the capture with the promotions", () => {
+          const fen = parseFen("4br2/k3P3/8/8/8/8/K7/8 w - - 0 1");
 
-        expect(getLegalMoves0x88(fen, E5_0x88)).toMatchMoves([
-          encodeMove0x88(E5_0x88, F4_0x88, NO_PIECE_0x88, CAPTURE_FLAG_0x88),
-        ]);
+          expect(getLegalMoves0x88(fen, E7_0x88)).toMatchMoves([
+            encodeMove0x88(E7_0x88, F8_0x88, WHITE_KNIGHT_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, F8_0x88, WHITE_BISHOP_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, F8_0x88, WHITE_ROOK_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E7_0x88, F8_0x88, WHITE_QUEEN_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
+      });
+
+      describe("when the player is black", () => {
+        it("returns the capture with the pormotions", () => {
+          const fen = parseFen("8/k7/8/8/8/8/K3p3/4BR2 b - - 0 1");
+
+          expect(getLegalMoves0x88(fen, E2_0x88)).toMatchMoves([
+            encodeMove0x88(E2_0x88, F1_0x88, BLACK_KNIGHT_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, F1_0x88, BLACK_BISHOP_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, F1_0x88, BLACK_ROOK_0x88, CAPTURE_FLAG_0x88),
+            encodeMove0x88(E2_0x88, F1_0x88, BLACK_QUEEN_0x88, CAPTURE_FLAG_0x88),
+          ]);
+        });
       });
     });
   });
